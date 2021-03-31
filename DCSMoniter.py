d65e500b4e68f42868dacce4badb7ac1fc765083
Binbin Dong <binbin.dong1994@gmail.com>
2021-03-31 14:14:57 -0500

    added Power Supply I/V monitering

--- DCSMoniter.py
+++ DCSMoniter.py
@@ -3,14 +3,16 @@ from influxdb import InfluxDBClient
 import datetime
 import time
 
-def upload(temp):
+def upload(temp, voltage, current):
     """influxdb info format"""
     data_list = [{
         'measurement': 'teststand',
         'tags': {'cpu': 'aspen'},
         'fields':{
             'time': datetime.datetime.now().strftime("%H:%M:%S"),
-            'temperature': temp
+            'temperature': temp,
+            'voltage': voltage,
+            'current': current
             }
         }]
 
@@ -21,11 +23,16 @@ client.switch_database('dcsDB')
 
 rm = visa.ResourceManager()
 DAQ = rm.open_resource('TCPIP::k-daq970a-01258.dhcp.okstate.edu::inst0::INSTR')
+PS = rm.open_resource('TCPIP::K-E36233A-09066.dhcp.okstate.edu::inst0::INSTR')
 print(DAQ.query('*IDN?'))
 
 while True:
     DAQ.write("MEAS:TEMP? (@101)")
     temp = float(DAQ.read())
-    print(temp)
-    client.write_points(upload(temp))
+    PS.write("MEAS:VOLT?")
+    Voltage = float(PS.read())
+    PS.write("MEAS:CURR?")
+    Current = float(PS.read())
+    print(temp, Voltage, Current)
+    client.write_points(upload(temp, Voltage, Current))
     time.sleep(3)
