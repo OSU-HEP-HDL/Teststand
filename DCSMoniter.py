50a9f2c7355f6df83aefce0995afe7c20f6e64e9
Binbin Dong <binbin.dong1994@gmail.com>
2021-03-31 14:27:06 -0500

    added Exception

--- DCSMoniter.py
+++ DCSMoniter.py
@@ -2,6 +2,7 @@ import pyvisa as visa
 from influxdb import InfluxDBClient
 import datetime
 import time
+import sys
 
 def upload(temp, voltage, current):
     """influxdb info format"""
@@ -21,10 +22,18 @@ def upload(temp, voltage, current):
 client = InfluxDBClient(host='localhost', port=8086)
 client.switch_database('dcsDB')
 
+DAQ_visa = 'TCPIP::k-daq970a-01258.dhcp.okstate.edu::inst0::INSTR'
+PS_visa = 'TCPIP::K-E36233A-09066.dhcp.okstate.edu::inst0::INSTR'
+
 rm = visa.ResourceManager()
-DAQ = rm.open_resource('TCPIP::k-daq970a-01258.dhcp.okstate.edu::inst0::INSTR')
-PS = rm.open_resource('TCPIP::K-E36233A-09066.dhcp.okstate.edu::inst0::INSTR')
-print(DAQ.query('*IDN?'))
+try:
+    DAQ = rm.open_resource(DAQ_visa)
+except vasa.Error as ex:
+    print("couldn't connect to DAQ. Please make sure your devide is on and the visa address is correct")
+try:
+    PS = rm.open_resource(PS_visa)
+except vasa.Error as ex:
+    print("couldn't connect to Power Supply. Please make sure your devide is on and the visa address is correct")
 
 while True:
     DAQ.write("MEAS:TEMP? (@101)")
