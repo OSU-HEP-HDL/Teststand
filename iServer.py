a1034395c0f24ff4f1316bf10f5441d2847c0251
Binbin Dong <binbin.dong1994@gmail.com>
2021-04-29 10:26:05 -0500

    correred received data format

--- iServer.py
+++ iServer.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 from time import time, sleep
+import re
 
 host = '10.206.68.18'
 # iServer ip address
@@ -9,23 +10,29 @@ def connectiServer(hostname, port, content):
     ''' connect to iServer and access temperature/RH'''
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((hostname, port))
-    s.sendall(content)
+    s.sendall(content.encode('utf-8'))
     s.shutdown(socket.SHUT_WR)
-    while True:
-        data = s.recv(1024)
-        if data == "":
-        sys.exit("no data received.")
+    data = s.recv(1024)
+    s.close()
+    return data
     
 
 def readiServer(hostname):
     ''' read temperature/RH value from iServer  '''
     temp = connectiServer(hostname, 1000, "*SRTF\r")
-    temp = temp[1:-1]
+    try:
+        temp = float(re.findall(r'[-+]?\d*\.\d+', temp.decode('utf-8'))[0])
+        print(temp)
+    except IndexError:
+        print('No temperature value found.')
+
     rh = connectiServer(hostname, 1000, "*SRH\r")
-    rh = rh[1:-1]
+    try:
+        rh = float(re.findall(r'[-+]?\d*\.\d+', rh.decode('utf-8'))[0])
+    except IndexError:
+        print('No humidity value found.')
     sleep(2)
     return temp, rh
 
 if __name__ == '__main__':
-        ts = ini(time())
         temp, rh = readiServer(host)
