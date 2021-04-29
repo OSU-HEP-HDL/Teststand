57f0bbcd49f6bf6a0da183c21d4a7b248e17046a
Binbin Dong <binbin.dong1994@gmail.com>
2021-04-29 13:08:45 -0500

    fetch master version

--- iServer.py
+++ iServer.py
@@ -1,6 +1,9 @@
 import socket
 import sys
-from time import time, sleep
+import time
+import datetime
+import re
+from influxdb import InfluxDBClient
 
 host = '10.206.68.18'
 # iServer ip address
@@ -9,23 +12,49 @@ def connectiServer(hostname, port, content):
     ''' connect to iServer and access temperature/RH'''
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((hostname, port))
-    s.sendall(content)
+    s.sendall(content.encode('utf-8'))
     s.shutdown(socket.SHUT_WR)
-    while True:
-        data = s.recv(1024)
-        if data == "":
-        sys.exit("no data received.")
+    data = s.recv(1024)
+    s.close()
+    return data
     
 
 def readiServer(hostname):
     ''' read temperature/RH value from iServer  '''
+
     temp = connectiServer(hostname, 1000, "*SRTF\r")
-    temp = temp[1:-1]
-    rh = connectiServer(hostname, 1000, "*SRH\r")
-    rh = rh[1:-1]
-    sleep(2)
+    while temp.decode('utf-8') is '':
+        temp = connectiServer(hostname, 1000, "*SRTF\r")
+    temp = float(re.findall(r'[-+]?\d*\.\d+', temp.decode('utf-8'))[0])
+
+    rh = connectiServer(hostname, 1000, "*SRH2\r")
+    while rh.decode('utf-8') is '':
+        rh = connectiServer(hostname, 1000, "*SRH2\r")
+    rh = float(re.findall(r'[-+]?\d*\.\d+', rh.decode('utf-8'))[0])
+
     return temp, rh
 
+def uploaddata(temperature, humidity):
+    """influxdb info format"""
+    data_list = [{
+        'measurement': 'teststand',
+        'tags': {'cpu': 'aspen'},
+        'fields':{
+            'time': datetime.datetime.now().strftime("%H:%M:%S"),
+            'iServer_temperature': temperature,
+            'iServer_rh': humidity
+            }
+        }]
+
+    return data_list
+
+
 if __name__ == '__main__':
-        ts = ini(time())
+    client = InfluxDBClient(host='localhost', port=8086)
+    client.switch_database('dcsDB')
+    while True:
         temp, rh = readiServer(host)
+        print('temperature: {} F, humidity = {} % RH '.format(temp, rh))
+        client.write_points(uploaddata(temp, rh))
+        time.sleep(3)
+
