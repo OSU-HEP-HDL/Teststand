adfcd10876899c82ffa155f2111af0a7a1f46d9c
Binbin Dong <binbin.dong1994@gmail.com>
2021-05-26 14:01:43 -0500

    added Module temperature measurement

--- PS_interlock.ino
+++ PS_interlock.ino
@@ -1,5 +1,21 @@
 #include <Wire.h>
 
+float Vin = 5.;
+float T[0];
+float V1;
+float R0=1180;
+float R01=10000;
+float R1, logR1;
+float c1=298.15, b1=3070., b2=3380.;
+
+float gettemp(){
+  V1 = (5./1023)*analogRead(A0);
+  R1 = V1*R0 / (Vin - V1);
+  logR1 = log(R1/R01);
+  T[0]=(1.0/(1/c1 + ((1/3380.)*logR1))) - 273.15;
+  return T[0];
+}
+
 void setup() {
   Wire.begin();
   Serial.begin(9600);
@@ -8,5 +24,16 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  analogWrite(A4, 0);
+  float moduleTemp;
+  moduleTemp = gettemp();
+  Serial.print(moduleTemp);
+  Serial.print("\n");
+  delay(1000);
+  if(moduleTemp > 30){
+    analogWrite(A4, 1023);
+  }
+  else{
+    analogWrite(A4,0);
+  }
+  
 }
